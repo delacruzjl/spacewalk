00107b8198cae4d83fe97010c0cbfeb21e55241d
Jason Foley <jason.s.foley.civ@mail.mil>
2018-10-18 14:34:20 -0400

    Removing node_modules

--- node_modules/docx/src/export/formatter.ts
+++
@@ -1,7 +0,0 @@
-import { BaseXmlComponent, IXmlableObject } from "file/xml-components";
-
-export class Formatter {
-    public format(input: BaseXmlComponent): IXmlableObject {
-        return input.prepForXml();
-    }
-}
--- node_modules/docx/src/file/xml-components/xmlable-object.ts
+++
@@ -1,9 +0,0 @@
-export interface IXmlableObject extends Object {
-    _attr?: { [key: string]: string | number | boolean };
-}
-
-// Needed because of: https://github.com/s-panferov/awesome-typescript-loader/issues/432
-/**
- * @ignore
- */
-export const WORKAROUND3 = "";
